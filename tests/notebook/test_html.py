d91b3dbf038bed75bbf3202cad0866a971406774
Daizu <daizutabi@gmail.com>
2025-04-07 13:10:08 +0000

    Add new test files for Holoviews, HTML, Seaborn, and Text notebook formats, implementing fixtures for notebook execution and data validation in pytest

--- tests/notebook/test_html.py
+++ tests/notebook/test_html.py
@@ -1,15 +1,20 @@
 import pytest
 
+from nbstore.notebook import Notebook
 from nbstore.store import Store
 
 
-@pytest.fixture(scope="module", autouse=True)
-def _execute(store: Store):
-    return store.execute("html.ipynb")
+@pytest.fixture(scope="module")
+def nb(store: Store):
+    nb = store.get_notebook("html.ipynb")
+    assert not nb.is_executed
+    nb.execute()
+    assert nb.is_executed
+    return nb
 
 
-def test_outputs(store: Store):
-    outputs = store.get_outputs("html.ipynb", "html:text")
+def test_outputs(nb: Notebook):
+    outputs = nb.get_outputs("html:text")
     assert isinstance(outputs, list)
     assert len(outputs) == 1
     assert isinstance(outputs[0], dict)
@@ -17,16 +22,16 @@ def test_outputs(store: Store):
     assert "text/html" in outputs[0]["data"]
 
 
-def test_data(store: Store):
-    data = store.get_data("html.ipynb", "html:text")
+def test_data(nb: Notebook):
+    data = nb.get_data("html:text")
     assert isinstance(data, dict)
     assert len(data) == 2
     assert "text/html" in data
     assert data["text/html"] == "<p><strong>Hello, World!</strong></p>"
 
 
-def test_mime_content(store: Store):
-    content = store.get_mime_content("html.ipynb", "html:text")
+def test_mime_content(nb: Notebook):
+    content = nb.get_mime_content("html:text")
     assert isinstance(content, tuple)
     assert len(content) == 2
     assert isinstance(content[1], str)
@@ -34,8 +39,8 @@ def test_mime_content(store: Store):
     assert content[1] == "<p><strong>Hello, World!</strong></p>"
 
 
-def test_mime_content_png(store: Store):
-    content = store.get_mime_content("html.ipynb", "html:png")
+def test_mime_content_png(nb: Notebook):
+    content = nb.get_mime_content("html:png")
     assert isinstance(content, tuple)
     assert len(content) == 2
     assert isinstance(content[1], str)
--- tests/notebook/test_text.py
+++ tests/notebook/test_text.py
@@ -1,44 +1,49 @@
 import pytest
 
+from nbstore.notebook import Notebook
 from nbstore.store import Store
 
 
-@pytest.fixture(scope="module", autouse=True)
-def _execute(store: Store):
-    return store.execute("text.ipynb")
+@pytest.fixture(scope="module")
+def nb(store: Store):
+    nb = store.get_notebook("text.ipynb")
+    assert not nb.is_executed
+    nb.execute()
+    assert nb.is_executed
+    return nb
 
 
-def test_mime_content_stdout(store: Store):
-    content = store.get_mime_content("text.ipynb", "text:stdout")
+def test_mime_content_stdout(nb: Notebook):
+    content = nb.get_mime_content("text:stdout")
     assert isinstance(content, tuple)
     assert content[0] == "text/plain"
     assert content[1] == "'stdout'"
 
 
-def test_mime_content_stream(store: Store):
-    content = store.get_mime_content("text.ipynb", "text:stream")
+def test_mime_content_stream(nb: Notebook):
+    content = nb.get_mime_content("text:stream")
     assert isinstance(content, tuple)
     assert content[0] == "text/plain"
     assert content[1] == "stream1\nstream2\n"
 
 
-def test_mime_content_both(store: Store):
-    content = store.get_mime_content("text.ipynb", "text:both")
+def test_mime_content_both(nb: Notebook):
+    content = nb.get_mime_content("text:both")
     assert isinstance(content, tuple)
     assert content[0] == "text/plain"
     assert content[1] == "'hello'"
 
 
-def test_mime_content_pandas(store: Store):
-    content = store.get_mime_content("text.ipynb", "text:pandas")
+def test_mime_content_pandas(nb: Notebook):
+    content = nb.get_mime_content("text:pandas")
     assert isinstance(content, tuple)
     assert content[0] == "text/html"
     assert isinstance(content[1], str)
     assert content[1].startswith("<div>")
 
 
-def test_mime_content_polars(store: Store):
-    content = store.get_mime_content("text.ipynb", "text:polars")
+def test_mime_content_polars(nb: Notebook):
+    content = nb.get_mime_content("text:polars")
     assert isinstance(content, tuple)
     assert content[0] == "text/html"
     assert isinstance(content[1], str)
