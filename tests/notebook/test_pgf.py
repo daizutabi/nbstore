5210c413e649db7aacbed8d3642725d5295a83db
Daizu <daizutabi@gmail.com>
2025-04-07 12:57:41 +0000

    Add test files for various notebook formats and implement fixtures for testing notebook execution in pytest

--- tests/notebook/test_pgf.py
+++ tests/notebook/test_pgf.py
@@ -1,28 +1,33 @@
 import pytest
 
+from nbstore.notebook import Notebook
 from nbstore.store import Store
 
 
-@pytest.fixture(scope="module", autouse=True)
-def _execute(store: Store):
-    return store.execute("pgf.ipynb")
+@pytest.fixture(scope="module")
+def nb(store: Store):
+    nb = store.get_notebook("pgf.ipynb")
+    assert not nb.is_executed
+    nb.execute()
+    assert nb.is_executed
+    return nb
 
 
-def test_cell(store: Store):
-    cell = store.get_cell("pgf.ipynb", "fig:pgf")
+def test_cell(nb: Notebook):
+    cell = nb.get_cell("fig:pgf")
     assert isinstance(cell, dict)
     assert "cell_type" in cell
 
 
-def test_source(store: Store):
-    source = store.get_source("pgf.ipynb", "fig:pgf")
+def test_source(nb: Notebook):
+    source = nb.get_source("fig:pgf")
     assert isinstance(source, str)
     assert source.startswith("import")
     assert "plot" in source
 
 
-def test_outputs(store: Store):
-    outputs = store.get_outputs("pgf.ipynb", "fig:pgf")
+def test_outputs(nb: Notebook):
+    outputs = nb.get_outputs("fig:pgf")
     assert isinstance(outputs, list)
     assert len(outputs) == 1
     assert isinstance(outputs[0], dict)
@@ -30,8 +35,8 @@ def test_outputs(store: Store):
     assert "text/plain" in outputs[0]["data"]
 
 
-def test_data(store: Store):
-    data = store.get_data("pgf.ipynb", "fig:pgf")
+def test_data(nb: Notebook):
+    data = nb.get_data("fig:pgf")
     assert isinstance(data, dict)
     assert len(data) == 2
     assert "text/plain" in data
@@ -39,5 +44,5 @@ def test_data(store: Store):
     assert data["text/plain"].startswith("%% Creator: Matplotlib,")
 
 
-def test_stream(store: Store):
-    assert store.get_stream("pgf.ipynb", "fig:stream") == "123\n"
+def test_stream(nb: Notebook):
+    assert nb.get_stream("fig:stream") == "123\n"
--- tests/store/test_store.py
+++ tests/store/test_store.py
@@ -55,11 +55,11 @@ def test_current_path_include_identifier(store: Store):
 
 def test_abs_path_error(store: Store):
     with pytest.raises(ValueError, match="Notebook not found"):
-        store.get_abs_path("unknown.ipynb")
+        store.find_path("unknown.ipynb")
 
 
 def test_abs_path_error_with_current_path(store: Store):
-    store.current_path = None
+    store.active_path = None
     with pytest.raises(ValueError, match="No active notebook."):
         store.get_notebook("")
 
