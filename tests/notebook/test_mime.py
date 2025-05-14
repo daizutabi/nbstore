74a074347a41f483acbb1521eb17601a59a09942
Daizu <daizutabi@gmail.com>
2025-05-14 10:00:06 +0000

    Add tests for SVG and text output in notebook MIME handling
    - Introduced new SVG files for testing text rendering with matplotlib.
    - Updated `test_mime.py` to include tests for retrieving and validating SVG and text MIME types.
    - Enhanced the notebook to demonstrate proper handling of different output formats.

--- tests/notebook/test_mime.py
+++ tests/notebook/test_mime.py
@@ -1,17 +1,35 @@
 from pathlib import Path
 
 import nbformat
+import pytest
 from nbformat import NotebookNode
 
 
-def test_language_kernel():
-    from nbstore.notebook import get_data
-
+@pytest.fixture(scope="module")
+def nb():
     path = Path(__file__).parent.joinpath("mime.ipynb")
     nb = nbformat.read(path, as_version=4)
     assert isinstance(nb, NotebookNode)
+    return nb
+
+
+def test_mime(nb: NotebookNode):
+    from nbstore.notebook import get_data
+
     data = get_data(nb, "plot")
     assert len(data) == 3
     assert "text/plain" in data
     assert "image/svg+xml" in data
+    assert "image/png" in data
+
+
+def test_text(nb: NotebookNode):
+    from nbstore.notebook import get_data
+
+    data = get_data(nb, "text")
+    assert len(data) == 3
+    assert "text/plain" in data
+    assert "image/svg+xml" in data
+    print(data["image/svg+xml"])
+    assert 0
     assert "image/png" in data
